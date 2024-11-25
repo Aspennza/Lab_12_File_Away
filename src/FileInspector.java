70edc33ce4156580f62d266741f504068d1e7ef5
Zoe Aspenns <aspennza@mail.uc.edu>
2024-11-25 11:58:43 -0500

    Program started; try/catch system established

---
+++ src/FileInspector.java
@@ -0,0 +1,56 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import static java.lang.System.out;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.CREATE;
+import javax.swing.JFileChooser;
+
+public class FileInspector
+{
+    //INCLUDE JAVADOC!!
+
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+                int line = 0;
+            }
+
+
+        }
+        catch (FileNotFoundException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+
+    }
+}
