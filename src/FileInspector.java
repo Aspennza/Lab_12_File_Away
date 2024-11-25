621e35c9df16dfb3162924cb7582fd2ddcfd3100
Zoe Aspenns <aspennza@mail.uc.edu>
2024-11-25 15:30:34 -0500

    Part 1 program draft complete

--- src/FileInspector.java
+++ src/FileInspector.java
@@ -10,9 +10,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import static java.lang.System.out;
+
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static java.nio.file.StandardOpenOption.CREATE;
 import javax.swing.JFileChooser;
 
@@ -25,6 +30,7 @@ public class FileInspector
         JFileChooser chooser = new JFileChooser();
         File selectedFile;
         String rec = "";
+        ArrayList<String> lines = new ArrayList<>();
 
         try {
             File workingDirectory = new File(System.getProperty("user.dir"));
@@ -35,21 +41,59 @@ public class FileInspector
                 selectedFile = chooser.getSelectedFile();
                 Path file = selectedFile.toPath();
 
+                Path fileName = file.getFileName();
+
                 InputStream in =
                         new BufferedInputStream(Files.newInputStream(file, CREATE));
                 BufferedReader reader =
                         new BufferedReader(new InputStreamReader(in));
 
                 int line = 0;
+                int totalChars = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                    totalChars = totalChars + rec.length();
+                    line++;
+
+                    System.out.printf("\n\nLine %4d %-60s ", line, rec);
+                }
+                reader.close();
+                System.out.println("\n\nThe data file has been read.");
+
+
+                //DOES THIS WORK??
+
+                String[] words;
+                ArrayList<String> result = new ArrayList<>();
+                for(String l:lines)
+                {
+                    words = l.split("\\s+");
+                    result.addAll(Arrays.asList(words));
+                }
+
+                System.out.println("\nSummary of processed file:");
+                System.out.printf("\nName of file processed: %50s", fileName);
+                System.out.printf("\nNumber of lines in file: %4d", line);
+                System.out.printf("\nNumber of words in file: %5d", result.size());
+                System.out.printf("\nNumber of characters in file: %5d", totalChars);
+
+            } else
+            {
+                System.out.println("The user didn't choose a file to process. Rerun the program to select and process a file.");
+                System.exit(0);
             }
-
-
         }
-        catch (FileNotFoundException e) {
-
+        catch (FileNotFoundException e)
+        {
+            System.out.println("The file couldn't be found.");
+            e.printStackTrace();
         }
-        catch (IOException e) {
-
+        catch (IOException e)
+        {
+            System.out.println("An exception occurred.");
+            e.printStackTrace();
         }
 
     }
